01925c9f69a6e015c33d801577f21a65dd563be4
camiloretro22-lgtm <camiloretro22@gmail.com>
2026-01-05 19:55:26 -0500

    Add files via upload
    proyecto sobre marcas tipos y modelos de placas de vehiculos en general

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-car_data = pd.read_csv('notebooks/vehicles_us.csv')
+car_data = pd.read_csv('vehicles_us.csv')
 hist_button = st.button('Construir histograma')
 
 if hist_button:
@@ -11,4 +11,3 @@ if hist_button:
     fig = px.histogram(car_data, x="odometer")
 
     st.plotly_chart(fig, use_container_width=True)
-
